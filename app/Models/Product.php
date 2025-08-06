d3cd7bebeabbf035ce4e98578c595d8d42d1cb31
M Abdus Shakur <mabdusshakur12@gmail.com>
2025-08-07 05:59:06 +0600

    Add Category & Order Model-Migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'category_name',
+        'image_url',
+        'price',
+    ];
+}
